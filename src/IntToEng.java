9a3d6e766578eab63b827c3c95cc74972d1b2224
g14906hr <g14906hr@s30407.tsuda.ac.jp>
2016-05-13 14:58:58 +0900

    ドライバーK子 ナビゲーター　R子 どこがどの数字を扱っているのかわからなくなったのでコメントをつけました 簡潔になる方法を考えたらパンクしました。

--- src/IntToEng.java
+++ src/IntToEng.java
@@ -26,47 +26,47 @@ public class IntToEng {
     	int m = n / 1000;
     	
     	if (n == 0) {
-    		return "zero";
+    		return "zero";//数字が０の時
     	} else if (j == 0) {
-    		return firstNumber[i];
+    		return firstNumber[i];//数字が０以外の一桁の時
     	} else if (j == 1) {
-    		return secondNumber[i];
+    		return secondNumber[i];//数字が１０から１９の時
     	} else if(j >= 2 && j <= 9){
-    		return thirdNumber[j - 2] + " " + firstNumber[i];
+    		return thirdNumber[j - 2] + " " + firstNumber[i];//数字が２０から９９の時
     	} else if(j >= 10 && j <= 99){
     		if(j % 10 == 0){
-    			return firstNumber[k] + "hundred "+firstNumber[i];
+    			return firstNumber[k] + "hundred "+firstNumber[i];//3桁の数字で１０の位が０の時
     		} else if(j % 10 == 1){
-    			return firstNumber[k] + "hundred "+secondNumber[i];
+    			return firstNumber[k] + "hundred "+secondNumber[i];//３桁の数字で１０の位が１の時
     		} else {
-    			return firstNumber[k] + "hundred "+thirdNumber[l / 10 - 2]+" "+firstNumber[i];
+    			return firstNumber[k] + "hundred "+thirdNumber[l / 10 - 2]+" "+firstNumber[i];//３桁の数字で１０の位が２から９の時
     		}
     	} else if (j >= 100) {
     		if(k % 10 == 0){
     			if (j % 10 == 0) {
-        			return firstNumber[m] + "thousand " + firstNumber[i];
+        			return firstNumber[m] + "thousand " + firstNumber[i];//4桁の数字で１００の位と１０の位が０の時
         		} else if (j % 10 == 1) {
-        			return firstNumber[m] + "thousand " + secondNumber[i];
+        			return firstNumber[m] + "thousand " + secondNumber[i];//４桁の数字で１００の位が０かつ１０の位が１の時
         		} else {
-        			return firstNumber[m] + "thousand "+thirdNumber[l / 10 - 2]+" "+firstNumber[i];
-        		}//100の位が０の時
+        			return firstNumber[m] + "thousand "+thirdNumber[l / 10 - 2]+" "+firstNumber[i];//４桁の数字で１００の位が０かつ１０の位が２から９の時
+        		}
     			
     		} else {
     			if (m == 1) {
     				if(l / 10 == 0){
-    					return secondNumber[k%10]+"hundred " + firstNumber[i];
+    					return secondNumber[k%10]+"hundred " + firstNumber[i];//４桁の数字で１０００の位が１で１０の位が０の時
     				} else if(l / 10 == 1){
-    					return secondNumber[k%10]+"hundred " + secondNumber[i];
+    					return secondNumber[k%10]+"hundred " + secondNumber[i];//４桁の数字で１０００の位が１で１０の位が１の時
     				} else {
-    					return secondNumber[k%10]+"hundred " + thirdNumber[l / 10 - 2] + " " + firstNumber[i];
+    					return secondNumber[k%10]+"hundred " + thirdNumber[l / 10 - 2] + " " + firstNumber[i];//４桁の数字で１０００の位が１で１０の位が２から９
     				}
     			} else {
     				if (j % 10 == 0) {
-    					return thirdNumber[m - 2] + " " + firstNumber[k % 10] + "hundred " + firstNumber[i];
+    					return thirdNumber[m - 2] + " " + firstNumber[k % 10] + "hundred " + firstNumber[i];//４桁の数字で１０００の位が２から９で１０の位が０の時
     				} else if (j % 10 == 1) {
-    					return thirdNumber[m - 2] + " " + firstNumber[k % 10] + "hundred " + secondNumber[i];
+    					return thirdNumber[m - 2] + " " + firstNumber[k % 10] + "hundred " + secondNumber[i];//４桁の数字で１０００の位が２から９で１０の位が1の時
     				} else {
-    					return thirdNumber[m - 2] + " " + firstNumber[k % 10] + "hundred " + thirdNumber[l / 10 - 2] + " " + firstNumber[i];
+    					return thirdNumber[m - 2] + " " + firstNumber[k % 10] + "hundred " + thirdNumber[l / 10 - 2] + " " + firstNumber[i];//４桁の数字で１０００の位が２から９で１０の位が２から９の時
     				}
     			}
     		}
