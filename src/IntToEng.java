f03358a4957e6070acc0f88f24f1e36f2f979668
g14906hr <g14906hr@s30407.tsuda.ac.jp>
2016-05-13 11:48:50 +0900

    ドライバー　K子 ナビゲーター　R子 １００から１９９までも表示できるようにしました

--- src/IntToEng.java
+++ src/IntToEng.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class IntToEng {
 
-	static String[] firstNumber = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+	static String[] firstNumber = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
 	static String[] secondNumber = {"ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
 	static String[] thirdNumber = {"twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
 	
@@ -21,19 +21,27 @@ public class IntToEng {
     	
     	int i = n % 10;
     	int j = n / 10;
+    	int k = n / 100;
+    	int l = n % 100;
     	
     	if (n == 0) {
     		return "zero";
     	} else if (j == 0) {
-    		return firstNumber[i -1];
+    		return firstNumber[i];
     	} else if (j == 1) {
     		return secondNumber[i];
-    	} else if (i == 0) {
-    		return thirdNumber[j - 2];
-    	} else {
-    		return thirdNumber[j - 2] + " " + firstNumber[i - 1];
+    	} else if(j >= 2 && j <= 9){
+    		return thirdNumber[j - 2] + " " + firstNumber[i];
+    	} else if(j >= 10){
+    		if(j == 10){
+    			return firstNumber[k] + "hundred "+firstNumber[i];
+    		} else if(j == 11){
+    			return firstNumber[k] + "hundred "+secondNumber[i];
+    		} else {
+    			return firstNumber[k] + "hundred "+thirdNumber[l / 10 - 2]+" "+firstNumber[i];
+    		}
     	}
-
+    	return "";
 	}
 
 }
